3b5fe8dcb2d9ada09e0df31c22d690f0abb098c0
재우 장 <jjang6632@gmail.com>
2023-10-06 00:27:49 +0900

    Flutter로 가위바위보 게임 만들기

---
+++ lib/game/widget/game_result.dart
@@ -0,0 +1,39 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:part2_rsp_game/game/enum.dart';
+
+class GameResult extends StatelessWidget {
+  final bool isDone;
+  final Result? result;
+  final VoidCallback callback;
+
+  const GameResult({required this.isDone, required this.callback, required this.result, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    if (isDone) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            result!.displayString,
+            style: const TextStyle(fontSize: 32),
+          ),
+          const SizedBox(height: 8),
+          TextButton(
+            child: const Text('다시하기', style: TextStyle(fontSize: 24),),
+            onPressed: () => callback.call(),
+          )
+        ],
+      );
+    }
+
+    return const Center(
+      child: Text(
+        '가위, 바위, 보 중 하나를 선택해 주세요.',
+        style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
